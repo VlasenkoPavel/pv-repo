0bd4a75b86eedb03aeb78d39c197ff1870187231
VlasenkoPavel <psvlasenko@gmail.com>
2018-02-13 17:37:55 +0300

    added typeORM in progress

--- nodeapp/dist/index.js
+++ nodeapp/dist/index.js
@@ -1,8 +1,25 @@
 "use strict";
+// import "reflect-metadata";
+// import { Aplication } from './aplication/aplication';
 Object.defineProperty(exports, "__esModule", { value: true });
+// const controllersDir = `${__dirname}/controllers`;
+// console.log(controllersDir);
+// const app = new Aplication;
+// app.start(controllersDir);
 require("reflect-metadata");
-const aplication_1 = require("./aplication/aplication");
-const controllersDir = `${__dirname}/controllers`;
-console.log(controllersDir);
-const app = new aplication_1.Aplication;
-app.start(controllersDir);
+const typeorm_1 = require("typeorm");
+const Photo_1 = require("./db/models/Photo");
+typeorm_1.createConnection({
+    type: "postgres",
+    host: "localhost",
+    username: "pvlasenko",
+    password: "ac3102",
+    database: "nodeapp",
+    entities: [
+        Photo_1.Photo
+    ],
+    synchronize: true,
+    logging: false
+}).then(connection => {
+    // here you can start to work with your entities
+}).catch(error => console.log(error));
--- nodeapp/src/index.ts
+++ nodeapp/src/index.ts
@@ -1,8 +1,27 @@
+// import "reflect-metadata";
+// import { Aplication } from './aplication/aplication';
+
+// const controllersDir = `${__dirname}/controllers`;
+// console.log(controllersDir);
+
+// const app = new Aplication;
+// app.start(controllersDir);
+
 import "reflect-metadata";
-import { Aplication } from './aplication/aplication';
+import { createConnection } from "typeorm";
+import { Photo } from "./db/models/Photo";
 
-const controllersDir = `${__dirname}/controllers`;
-console.log(controllersDir);
-
-const app = new Aplication;
-app.start(controllersDir);
+createConnection({
+    type: "postgres",
+    host: "localhost",
+    username: "pvlasenko",
+    password: "ac3102",
+    database: "nodeapp",
+    entities: [
+        Photo
+    ],
+    synchronize: true,
+    logging: false
+}).then(connection => {
+    // here you can start to work with your entities
+}).catch(error => console.log(error));
