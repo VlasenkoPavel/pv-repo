097acc87d5928507591a70492af66b8a4e9071c6
VlasenkoPavel <psvlasenko@gmail.com>
2018-02-15 15:19:44 +0300

    add docker commands file

--- nodeapp/src/index.ts
+++
@@ -1,6 +0,0 @@
-import "reflect-metadata";
-import { Aplication } from './core/aplication/aplication';
-
-const controllersDir = `${__dirname}/controllers`;
-const app = new Aplication(controllersDir);
-app.start();
